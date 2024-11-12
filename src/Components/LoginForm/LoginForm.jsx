4ed76a779422bc003a3b44328057b97727a7a21b
DionCstS <dionathacastro1@gmail.com>
2024-11-12 10:47:40 -0300

    criacao de estrutura basica do formulario

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,6 +3,32 @@ import './LoginForm.css';
 
 export const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <div className='wrapper'>
+        <form action="">
+
+            <h1>Login</h1>
+
+            <div className="input-box">
+                <input type="text" placeholder='Usuario'/>
+            </div>
+
+            <div className="input-box">
+                <input type="text" placeholder='Usuario'/>
+            </div>
+
+            <div className="remember-forgot">
+              <label><input type="checkbox" />Lembrar-me</label>
+                <a href="#">Esqueceu a senha?</a>
+            </div>
+
+            <button type="submit">Login</button>
+
+            <div className="register-link">
+              <p>Não tem uma Conta?<a href="#">Registrar</a></p>
+            </div>
+
+
+        </form>
+    </div>
   )
 }
